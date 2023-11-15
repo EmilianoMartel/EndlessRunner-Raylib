893885027c8762ce3013611452a6d65e1faacde6
Emiliano <alohacocki@gmail.com>
2023-11-15 00:03:15 -0300

    Implemented WinOrLoseLogic

--- InfiniteWalk/Program.cs
+++ InfiniteWalk/Program.cs
@@ -8,6 +8,7 @@ class Program
         public Vector3 position;
         public Vector3 size;
         public Color color;
+        public BoundingBox bounds;
         public bool isActive;
     }
 
@@ -42,28 +43,38 @@ class Program
     const int DIFF_Z_SPAWN = 100;
 
     //Obstacles
-    private static RectangleCharacter[] rectangleObstaclesArray = new RectangleCharacter[10];
-    private static readonly Vector3[] sizeRectanglesArray = { new Vector3(5, 2.5f, 2), new Vector3(5, 2.5f, 2) };
-    private static readonly Color[] colorRectanglesArray = { Color.GREEN, Color.YELLOW };
+    private static RectangleCharacter[] obstaclesArray = new RectangleCharacter[10];
+    private static readonly Vector3[] SIZE_OBSTACLES_ARRAY = { new Vector3(5, 2.5f, 2), new Vector3(5, 2.5f, 2) };
+    private static readonly Color[] COLOR_OBSTACLES_ARRAY = { Color.BLUE, Color.DARKBLUE };
     private static float timerObstacles = 0;
     const float MIN_TIME_OBSTACLE = 2f;
     const float MAX_TIME_OBSTACLE = 4f;
     private static float timeBetweenObstacles = 2;
 
     //Coins
-    private static SphereCharacter[] sphereCoinsArray = new SphereCharacter[10];
+    private static SphereCharacter[] coinsArray = new SphereCharacter[10];
+    private static readonly float COIN_SIZE = 0.5f;
+    private static readonly Color COIN_COLOR = Color.YELLOW;
+    const float MIN_TIME_COIN = 2f;
+    const float MAX_TIME_COUN = 4f;
+    private static float timerCoins = 0;
+    private static float timeBetweenCoins = 2;
 
     //Ground
     private static Vector3 groundPosition = new Vector3(0.0f, 0.0f, 0.0f);
-    private static Vector2 groundSize = new Vector2(128.0f, 128.0f);
+    private static Vector2 groundSize = new Vector2(128.0f, 700.0f);
 
     //LimitWalls
     private static RectangleCharacter wallLeft;
     private static RectangleCharacter wallRight;
-    private static Vector3 wallSize = new Vector3(1.0f, 5.0f, 700.0f);
+    private static RectangleCharacter wallWin;
+    private static readonly Vector3 WALL_WIN_SIZE = new Vector3(20.0f, 5.0f, 1.0f);
+    private static readonly Vector3 WALL_LIMIT_SIZE = new Vector3(1.0f, 5.0f, 700.0f);
     private static Color colorWall = Color.RED;
-    private static Vector3 wallLeftPosition = new Vector3(-10.0f, 2.5f, 0.0f);
-    private static Vector3 wallRightPosition = new Vector3(10.0f, 2.5f, 0.0f);
+    private static Color winWallColor = Color.GREEN;
+    private static readonly Vector3 WALL_WIN_POSITION = new Vector3(0.0f, 2.5f, -200.0f);
+    private static readonly Vector3 WALL_LEFT_POSITION = new Vector3(-10.0f, 2.5f, 0.0f);
+    private static readonly Vector3 WALL_RIGHT_POSITION = new Vector3(10.0f, 2.5f, 0.0f);
 
     public static void Main()
     {
@@ -81,15 +92,24 @@ class Program
         player.lineColor = Color.DARKPURPLE;
 
         //Instanciate walls
-        wallLeft.position = wallLeftPosition;
-        wallLeft.size = wallSize;
+        wallLeft.position = WALL_LEFT_POSITION;
+        wallLeft.size = WALL_LIMIT_SIZE;
         wallLeft.color = colorWall;
-        wallRight.position = wallRightPosition;
-        wallRight.size = wallSize;
+        wallLeft.bounds = RectangleBounds(wallLeft);
+        wallRight.position = WALL_RIGHT_POSITION;
+        wallRight.size = WALL_LIMIT_SIZE;
         wallRight.color = colorWall;
+        wallRight.bounds = RectangleBounds(wallRight);
+        wallWin.position = WALL_WIN_POSITION;
+        wallWin.size = WALL_WIN_SIZE;
+        wallWin.color = winWallColor;
+        wallWin.bounds = RectangleBounds(wallWin);
 
         //Instanciate Obstacles
         CreateObstacles();
+
+        //Instanciate Coins
+        CreateCoins();
 
         while (!Raylib.WindowShouldClose())
         {
@@ -104,14 +124,25 @@ class Program
             UpdatePlayer();
             UpdateCameraPosition();
             UpdateObstacles();
+            UpdateCoins();
 
             // Draw ground
-            Raylib.DrawPlane(new Vector3(0.0f, 0.0f, 0.0f), new Vector2(128.0f, 128.0f), Color.LIGHTGRAY);
+            Raylib.DrawPlane(groundPosition, groundSize, Color.LIGHTGRAY);
 
             // Draw a walls
             Raylib.DrawCube(wallLeft.position, wallLeft.size.X, wallLeft.size.Y, wallLeft.size.Z, wallLeft.color);
             Raylib.DrawCube(wallRight.position, wallLeft.size.X, wallLeft.size.Y, wallLeft.size.Z, wallRight.color);
+            Raylib.DrawCube(wallWin.position, wallWin.size.X, wallWin.size.Y, wallWin.size.Z, wallWin.color);
 
+            //CheckCollisions
+            if (Raylib.CheckCollisionBoxSphere(wallRight.bounds, player.position, player.size) || Raylib.CheckCollisionBoxSphere(wallLeft.bounds, player.position, player.size))
+            {
+                WinOrLoseLogic(false);
+            }
+            if (Raylib.CheckCollisionBoxSphere(wallWin.bounds, player.position, player.size))
+            {
+                WinOrLoseLogic(true);
+            }
             Raylib.EndMode3D();
 
             //Draw UI
@@ -152,29 +183,60 @@ class Program
         DrawGameSphere(player);
     }
 
+    private static void CreateCoins()
+    {
+        for (int i = 0; i < coinsArray.Length; i++)
+        {
+            coinsArray[i].size = COIN_SIZE;
+            coinsArray[i].color = COIN_COLOR;
+        }
+    }
+
+    private static void UpdateCoins()
+    {
+
+    }
+
     private static void CreateObstacles()
     {
-        for (int i = 0; i < rectangleObstaclesArray.Length; i++)
+        for (int i = 0; i < obstaclesArray.Length; i++)
         {
-            int randomIndex = random.Next(0, sizeRectanglesArray.Length);
-            rectangleObstaclesArray[i].size = sizeRectanglesArray[randomIndex];
+            int randomIndex = random.Next(0, SIZE_OBSTACLES_ARRAY.Length);
+            obstaclesArray[i].size = SIZE_OBSTACLES_ARRAY[randomIndex];
         }
     }
 
     private static void UpdateObstacles()
     {
-        for (int i = 0; i < rectangleObstaclesArray.Length; i++)
+        for (int i = 0; i < obstaclesArray.Length; i++)
         {
-            if (rectangleObstaclesArray[i].isActive)
+            if (obstaclesArray[i].isActive)
             {
-                rectangleObstaclesArray[i].position.Z += delta * PLAYER_SPEED;
-                Raylib.DrawCube(rectangleObstaclesArray[i].position, rectangleObstaclesArray[i].size.X, rectangleObstaclesArray[i].size.Y, rectangleObstaclesArray[i].size.Z, rectangleObstaclesArray[i].color);
+                obstaclesArray[i].position.Z += delta * PLAYER_SPEED;
+                Raylib.DrawCube(obstaclesArray[i].position, obstaclesArray[i].size.X, obstaclesArray[i].size.Y, obstaclesArray[i].size.Z, obstaclesArray[i].color);
+                obstaclesArray[i].bounds = RectangleBounds(obstaclesArray[i]);
+                if (Raylib.CheckCollisionBoxSphere(obstaclesArray[i].bounds, player.position, player.size))
+                {
+                    WinOrLoseLogic(false);
+                }
             }
-            if (rectangleObstaclesArray[i].position.Z > player.position.Z + 2)
+            if (obstaclesArray[i].position.Z >= player.position.Z)
             {
-                rectangleObstaclesArray[i].isActive = false;
-                rectangleObstaclesArray[i].position = player.position + new Vector3(0,0,DIFF_Z_SPAWN);
+                obstaclesArray[i].isActive = false;
+                obstaclesArray[i].position = player.position + new Vector3(0, 0, DIFF_Z_SPAWN);
             }
+        }
+    }
+
+    private static void WinOrLoseLogic(bool isWinner)
+    {
+        if (isWinner)
+        {
+
+        }
+        else
+        {
+
         }
     }
 
@@ -183,19 +245,18 @@ class Program
         timerObstacles += Raylib.GetFrameTime();
         if (timerObstacles >= timeBetweenObstacles)
         {
-            Console.WriteLine("SpawnObstacles is calling");
             timeBetweenObstacles = (float)(random.NextDouble() * (MIN_TIME_OBSTACLE - MAX_TIME_OBSTACLE) + MIN_TIME_OBSTACLE);
             timerObstacles = 0;
             int positionX = random.Next((int)wallLeft.position.X, (int)wallRight.position.X);
-            Vector3 positionObstacle = new Vector3(positionX,1f,player.position.Z - DIFF_Z_SPAWN);
-            for (int i = 0; i < rectangleObstaclesArray.Length; i++)
+            Vector3 positionObstacle = new Vector3(positionX, 1f, player.position.Z - DIFF_Z_SPAWN);
+            for (int i = 0; i < obstaclesArray.Length; i++)
             {
-                int index = random.Next(0, colorRectanglesArray.Length);
-                if (!rectangleObstaclesArray[i].isActive)
+                int index = random.Next(0, COLOR_OBSTACLES_ARRAY.Length);
+                if (!obstaclesArray[i].isActive)
                 {
-                    rectangleObstaclesArray[i].isActive = true;
-                    rectangleObstaclesArray[i].position = positionObstacle;
-                    rectangleObstaclesArray[i].color = colorRectanglesArray[index];
+                    obstaclesArray[i].isActive = true;
+                    obstaclesArray[i].position = positionObstacle;
+                    obstaclesArray[i].color = COLOR_OBSTACLES_ARRAY[index];
                     return;
                 }
             }
